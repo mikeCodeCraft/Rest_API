a440db51f209cde7e2f275c7150da0b29c2ee321
michael <michaelokon565@gmail.com>
2025-05-06 16:17:36 +0100

    I created the profile model for the user

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,24 @@
+import os
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.deconstruct import deconstructible
 
-# Create your models here.
+@deconstructible
+class GenerateProfileImagePath(object):
+    def __init__(self):
+        pass
+
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        path = f'media/accounts/{instance.user.id}/images/'
+        name = f'profile_image.{ext}'
+        return os.path.join(path, name)
+    
+user_profile_image_path = GenerateProfileImagePath()
+class Profile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    image = models.FileField(upload_to= user_profile_image_path, default='default.jpg', blank=True,null=True)
+    
+    def __str__(self):
+        return f'{self.user.username}\'s profile'
+    
