9ad1c08a0d34e64ef157832e470369cdc188c0b2
michael <michaelokon565@gmail.com>
2025-05-31 14:10:18 +0100

    I did some setup i the settings

--- src/taskly_app/settings.py
+++ src/taskly_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_8^qtve%(2sjc*lt5=4^qq@%n^f+r8*j-8u^_o8tv%!xov8(0^'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower == "true"
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -108,7 +109,8 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.parse("postgresql://restapi_3jlw_user:FdDyerf8NZC33QQoOHs6yy5WpzVroA3k@dpg-d0t32pmmcj7s73b5acig-a.oregon-postgres.render.com/restapi_3jlw")
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- src/users/serializers.py
+++ src/users/serializers.py
@@ -36,6 +36,7 @@ class UserSerializer(serializers.ModelSerializer):
         
     def create(self, validated_data):
         password = validated_data.pop('password')
+        validated_data.pop('old_password', None) 
         user = User.objects.create(**validated_data)
         user.set_password(password)
         user.save()
